3b8fde496499bff00f0f01596efdca6aaefe547c
geraldmccormack <2635287+geraldmccormack@users.noreply.github.com>
2020-06-19 01:27:53 -0400

    gm619-3 Run Selenium UI tests

--- Tailspin.SpaceGame.Web.UITests/HomePageTest.cs
+++ Tailspin.SpaceGame.Web.UITests/HomePageTest.cs
@@ -91,6 +91,8 @@ namespace UITests
         // Download game
         [TestCase("download-btn", "pretend-modal")]
         // Screen image
+        [TestCase("screen-dfdfd1", "screen-modal")]
+        // // fail test Top player on the leaderboard
         [TestCase("screen-01", "screen-modal")]
         // // Top player on the leaderboard
         [TestCase("profile-1", "profile-modal-1")]
